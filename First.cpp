98a4c26791a5519c4265bd22833ff86d23cfa626
PramishSapkota <pramish60@gmail.com>
2023-05-17 21:09:09 +0545

    First Commit

---
+++ First.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class room{
+    int length;
+    int breadth;
+    int height;
+    void calculateArea(int a,int b){
+        cout<<"Area is"<<a*b;
+    }
+    void calculateVolume(int a,int b, int c){
+        cout<<"Area is"<<a*b*c;
+    }
+};
+int main(){
+    room lec
+    cout<<"Enter length";
+    cin>>lec.length;
+    cout<<"Enter breadth";
+    cin>>lec.breadth;
+    cout<<"Enter height";
+    cin>>lec.height;
+    void calculateArea(lec.length,lec.breadth);
+    void calculateVolume(lec.length,lec.breadth,lec.height);
+    return 0;
+}
