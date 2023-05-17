253816617409369a6538a222f13483968455b607
PramishSapkota <pramish60@gmail.com>
2023-05-17 22:44:02 +0545

    First Commit

---
+++ class.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    string name;
+    int roll;
+    int age;
+    void function(){
+       cout<<"Your name is";
+    }
+};  
+int main(){
+    student student1;
+    void function(){
+        cout<<"Enter your name";
+    cin>>student1.name;
+     cout<<"Enter your RollNo.";
+    cin>>student1.roll;
+     cout<<"Enter your age";
+    cin>>student1.age;
+    }
+    return 0;
+    } 
