e2c978f61d947762c4f5778fb12193bf8b7b4a04
PramishSapkota <pramish60@gmail.com>
2023-05-22 08:55:38 +0545

    second commit

--- Constructor.cpp
+++ Constructor.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 class Account {
  public:
- Account (int length, int breadth){
-     cout<<length*breadth;
+ Account (int length, int breadth){//making function name same as class name is constructor 
+     cout<<length*breadth;     //constructor doesnt have return type
 }
-Account(int x,int y,int z){
+Account(int x,int y,int z){//multiple constructor can be made but each should take different no. of arguments
     cout<<"hello";
 }
-
 };
 int main() {
- Account a1(10,20);
- Account a2(10,20,30);
+ Account a1(10,20);//this calls the function receiving 2 arguments
+ Account a2(10,20,30);//this calls the function receiving 3 arguments
   return 0;
 }
-
--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,10 +1,12 @@
+//main thing to grasp is derived class can access property of base class but vice versa isn't possible 
+//more in cmt of line 18
 #include <iostream>
 using namespace std;
-class Account{
+class Account{//base class
     public:
     float salary=60000;
 };
-class programmer: public Account{
+class programmer: public Account{//derived class
     public:
     float bonus=5000;
 };
@@ -12,7 +14,8 @@ int main(){
     programmer p1;
     Account a1;
     cout<<"Salary="<<p1.salary<<endl;
-    cout<<"Salary="<<a1.salary<<endl;
-    cout<<"Bonus="<<p1.bonus<<endl;
+    cout<<"Salary="<<a1.salary<<"\n";//"\n"&endl does same job
+    cout<<"Bonus="<<p1.bonus<<endl; //cout<<"Bonus="<<a1.bonus; doesnt work as account(base class) cant 
+                             //access bonus property[which is property of programmer(derived class)]
     return 0;
 }
