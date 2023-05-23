272a08295dbe89e02b09ab976d13b8bac685c1c3
PramishSapkota <pramish60@gmail.com>
2023-05-23 13:47:36 +0545

    First Chapter

---
+++ 1_practice/3_functionoverload.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   double m,n;
+   cout<<"Enter two numbers ";
+   cin>>a,b,;
+   cout<<"Enter two numbers ";
+   int add(a,b);
+   int add(m,n)
+   return 0;
+}
+int add(int i,int j){
+  cout<<"Sum of 1st two numbers=";
+  cout<<i+j<<endl;
+  return 0;
+}
+int add(double i,double j){
+    cout<<"Sum of all";
+    cout<<i+j;
+    return 0;
+}
