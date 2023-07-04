bbc55a153515d92632b665bb34ca30ea3dc6c3e9
PramishSapkota <pramish60@gmail.com>
2023-07-04 10:02:10 +0545

    Class

--- class_object/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-cout<<"\nOi Espali ghumna jaam bujhis "<<name<<" umm "
-            // <<age<<"ko umer ma hike garna sakinchha masta. ka jane hola ta "
-            // <<addresss<<" jada thik hola?";
